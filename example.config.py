5c5c2eb8f65e88ae6e3aedee024cf4ce8a9c7a47
Gloridust <Gloridust@gmail.com>
2024-08-31 00:49:32 +0800

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,9 +1,10 @@
 # config.py
 import os
+import tempfile
 
 # Git repository configuration
 GIT_REPO_URL = 'https://github.com/Gloridust/picsite.git'
-GIT_LOCAL_PATH = os.path.join(os.getcwd(), 'local_repo')
+GIT_LOCAL_PATH = tempfile.mkdtemp(prefix="picsite_repo_")
 
 # File paths
 ALBUMS_PATH = os.path.join(GIT_LOCAL_PATH, 'src', 'content', 'albums')
